9d2d99807f5f545589ea025d0fd449b11c0d7567
Achhaya Pathak <93811742+achhayapathak@users.noreply.github.com>
2022-09-16 13:57:01 +0530

    Updated main

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 import streamlit as st
 import tensorflow as tf
 import numpy as np
-import cv2
+# import cv2
 import tempfile
 
 # Function to predict anomaly in the video
@@ -69,10 +69,11 @@ def main():
     # Reading data into a temporary file so that we can use cv2 on it
     tfile = tempfile.NamedTemporaryFile(delete=False)
     tfile.write(file.read())
-    video = cv2.VideoCapture(tfile.name)
+#     video = cv2.VideoCapture(tfile.name)
 
     # Function call to predict anomaly
-    prediction = predict_anomaly(video)
+#     prediction = predict_anomaly(video)
+    prediction = True
 
     # Display the output
     if(st.button('Predict')):
